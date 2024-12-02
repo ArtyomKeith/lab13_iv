4b91c864d877344085fb3ec7e1a177d0f0296a40
ArtyomKeith <kekulya00@gmail.com>
2024-12-02 11:47:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,63 +2,52 @@ import streamlit as st
 import folium
 import requests
 from streamlit_folium import folium_static
-from folium.plugins import Search, MarkerCluster
-from geopy.distance import geodesic
 
 # Загружаем GeoJSON с GitHub
 url = "https://raw.githubusercontent.com/ArtyomKeith/lab13_iv/main/campus.geojson"
 geojson_data = requests.get(url).json()
 
 # Функция для отображения карты с приближением на выбранное здание
-def create_map(selected_building=None, map_type='OpenStreetMap', building_a=None, building_b=None):
-    # Инициализация карты с возможностью переключения между картами
+def create_map(selected_building=None, map_type="light", building_a=None, building_b=None):
+    # Инициализация карты
     m = folium.Map(location=[51.1879, 71.4085], zoom_start=16, control_scale=True)
-
-    # Добавление карт: OpenStreetMap, Esri, Google Maps
-    folium.TileLayer(map_type).add_to(m)
-
-    # Инициализация кластера маркеров
-    marker_cluster = MarkerCluster().add_to(m)
 
     # Добавление GeoJSON данных на карту
     folium.GeoJson(geojson_data, name="Campus").add_to(m)
 
-    # Добавление маркеров для каждого здания
-    for feature in geojson_data['features']:
-        building_name = feature['properties']['name']
-        coords = feature['geometry']['coordinates']
+    # Проверяем, если выбраны два здания, извлекаем их координаты
+    if building_a and building_b:
+        lat_a, lon_a, lat_b, lon_b = None, None, None, None
         
-        # Если это точка (Point), то координаты будут в формате [lon, lat]
-        if feature['geometry']['type'] == 'Point':
-            lat, lon = coords[1], coords[0]
+        for feature in geojson_data['features']:
+            # Проверяем, совпадает ли имя с building_a и building_b
+            if feature['properties']['name'] == building_a:
+                coords_a = feature['geometry']['coordinates']
+                if feature['geometry']['type'] == 'Point':
+                    lat_a, lon_a = coords_a[1], coords_a[0]
+
+            if feature['properties']['name'] == building_b:
+                coords_b = feature['geometry']['coordinates']
+                if feature['geometry']['type'] == 'Point':
+                    lat_b, lon_b = coords_b[1], coords_b[0]
+
+        if lat_a and lat_b:
+            # Добавляем маркеры для зданий A и B
+            folium.Marker([lat_a, lon_a], popup=building_a).add_to(m)
+            folium.Marker([lat_b, lon_b], popup=building_b).add_to(m)
             
-            # Добавляем маркер с попапом
-            popup_content = f"<strong>{building_name}</strong><br>Подробнее о здании..."
-            folium.Marker([lat, lon], popup=popup_content).add_to(marker_cluster)
-            
-            # Приближаем к этому зданию, если оно выбрано
-            if selected_building == building_name:
-                m.fit_bounds([[lat - 0.002, lon - 0.002], [lat + 0.002, lon + 0.002]])
+            # Приближаем карту, чтобы включить оба здания
+            m.fit_bounds([[lat_a - 0.002, lon_a - 0.002], [lat_b + 0.002, lon_b + 0.002]])
 
-            # Если указаны два здания, строим маршрут между ними
-            if building_a and building_b:
-                if building_a == building_name or building_b == building_name:
-                    if building_a == building_name:
-                        lat_a, lon_a = lat, lon
-                    if building_b == building_name:
-                        lat_b, lon_b = lat, lon
-                        
-    # Строим маршрут между двумя зданиями
-    if building_a and building_b and lat_a and lat_b:
-        folium.PolyLine([(lat_a, lon_a), (lat_b, lon_b)], color='blue', weight=2.5, opacity=1).add_to(m)
-
-        # Вычисляем расстояние между двумя зданиями
-        distance = geodesic((lat_a, lon_a), (lat_b, lon_b)).km
-        folium.Marker([(lat_a + lat_b) / 2, (lon_a + lon_b) / 2],
-                      popup=f"Расстояние: {distance:.2f} км").add_to(m)
-
-    # Поиск по названию зданий
-    search = Search(layer=folium.GeoJson(geojson_data)).add_to(m)
+    # Если выбрано одно здание, приближаем только его
+    elif selected_building:
+        for feature in geojson_data['features']:
+            if feature['properties']['name'] == selected_building:
+                coords = feature['geometry']['coordinates']
+                if feature['geometry']['type'] == 'Point':
+                    lat, lon = coords[1], coords[0]
+                    folium.Marker([lat, lon], popup=selected_building).add_to(m)
+                    m.fit_bounds([[lat - 0.002, lon - 0.002], [lat + 0.002, lon + 0.002]])
 
     # Отображаем карту
     folium_static(m)
@@ -73,19 +62,17 @@ st.markdown("""
     <h1 style="text-align: center;">Кампус Казахский агротехнический исследовательский университет имени С. Сейфуллина</h1>
 """, unsafe_allow_html=True)
 
-# Выбор типа карты
-map_type = st.selectbox("Выберите тип карты", ['OpenStreetMap', 'Esri', 'Stamen Terrain'])
-
 # Выпадающий список для выбора здания
-building = st.selectbox('Выберите здание', ['Все'] + [feature['properties']['name'] for feature in geojson_data['features']])
+building_a = st.selectbox('Выберите первое здание', ['Все'] + [feature['properties']['name'] for feature in geojson_data['features']])
+building_b = st.selectbox('Выберите второе здание', ['Все'] + [feature['properties']['name'] for feature in geojson_data['features']])
 
-# Выпадающие списки для выбора зданий для маршрута
-building_a = st.selectbox('Выберите первое здание для маршрута', ['Все'] + [feature['properties']['name'] for feature in geojson_data['features']])
-building_b = st.selectbox('Выберите второе здание для маршрута', ['Все'] + [feature['properties']['name'] for feature in geojson_data['features']])
-
-# Если выбрано здание, создаем карту с приближением
-if building != 'Все':
-    create_map(selected_building=building, map_type=map_type, building_a=building_a if building_a != 'Все' else None, building_b=building_b if building_b != 'Все' else None)
+# Если выбраны здания, создаем карту с приближением
+if building_a != 'Все' and building_b != 'Все':
+    create_map(building_a=building_a, building_b=building_b)
 else:
-    # Если не выбрано конкретное здание, показываем все
-    create_map(map_type=map_type, building_a=building_a if building_a != 'Все' else None, building_b=building_b if building_b != 'Все' else None)
+    # Если не выбраны конкретные здания, показываем все
+    selected_building = st.selectbox('Выберите здание для приближения', ['Все'] + [feature['properties']['name'] for feature in geojson_data['features']])
+    if selected_building != 'Все':
+        create_map(selected_building=selected_building)
+    else:
+        create_map()
