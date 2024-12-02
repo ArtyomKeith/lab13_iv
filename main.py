a932f5b9290c9d2450f144485702f625adaa0ab4
ArtyomKeith <kekulya00@gmail.com>
2024-12-02 11:26:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,76 +9,11 @@ geojson_data = requests.get(url).json()
 
 # Функция для отображения карты с приближением на выбранное здание
 def create_map(selected_building=None):
-    # Инициализация карты с использованием карт Microsoft Azure
-    m = folium.Map(location=[51.1879, 71.4085], zoom_start=16, control_scale=True)
-    
-    # Добавляем слой Microsoft Azure карты
-    folium.TileLayer(
-        tiles='https://ecn.t1.tiles.virtualearth.net/tiles/r{q}.jpeg?g=1',  # URL для карты Microsoft Azure
-        attr='&copy; <a href="https://www.microsoft.com/en-us/maps">Microsoft Azure</a>',
-        name='Microsoft Azure',
-        overlay=False,
-        control=True
-    ).add_to(m)
-
-    # Добавление GeoJSON данных на карту
-    folium.GeoJson(geojson_data, name="Campus").add_to(m)import streamlit as st
-import folium
-import requests
-from streamlit_folium import folium_static
-
-# Загружаем GeoJSON с GitHub
-url = "https://raw.githubusercontent.com/ArtyomKeith/lab13_iv/main/campus.geojson"
-geojson_data = requests.get(url).json()
-
-# Функция для отображения карты с приближением на выбранное здание
-def create_map(selected_building=None):
     # Инициализация карты
     m = folium.Map(location=[51.1879, 71.4085], zoom_start=16, control_scale=True)
 
     # Добавление GeoJSON данных на карту
     folium.GeoJson(geojson_data, name="Campus").add_to(m)
-
-    # Если выбрано здание, находим его и приближаем
-    if selected_building:
-        for feature in geojson_data['features']:
-            if feature['properties']['name'] == selected_building:
-                # Извлекаем координаты здания
-                coords = feature['geometry']['coordinates']
-                
-                # Если это точка (Point), то координаты будут в формате [lon, lat]
-                if feature['geometry']['type'] == 'Point':
-                    lat, lon = coords[1], coords[0]
-                    # Добавляем маркер на здание
-                    folium.Marker([lat, lon], popup=feature['properties']['name']).add_to(m)
-
-                    # Приближаем к этому зданию, чтобы здание было в центре экрана
-                    m.fit_bounds([[lat - 0.002, lon - 0.002], [lat + 0.002, lon + 0.002]])
-
-                # Если это полигоны (например, здания с несколькими углами), то извлекаем координаты для этих полигонов
-                elif feature['geometry']['type'] == 'Polygon':
-                    # Получаем минимальные и максимальные координаты полигона для fit_bounds
-                    latitudes = [coord[1] for coord in coords[0]]
-                    longitudes = [coord[0] for coord in coords[0]]
-                    bounds = [[min(latitudes), min(longitudes)], [max(latitudes), max(longitudes)]]
-                    m.fit_bounds(bounds)
-
-    # Отображаем карту
-    folium_static(m)
-
-# Заголовок
-st.title('Кампус Казахский агротехнический исследовательский университет имени С. Сейфуллина')
-
-# Выпадающий список для выбора здания
-building = st.selectbox('Выберите здание', ['Все'] + [feature['properties']['name'] for feature in geojson_data['features']])
-
-# Если выбрано здание, создаем карту с приближением
-if building != 'Все':
-    create_map(building)
-else:
-    # Если не выбрано конкретное здание, показываем все
-    create_map()
-
 
     # Если выбрано здание, находим его и приближаем
     if selected_building:
