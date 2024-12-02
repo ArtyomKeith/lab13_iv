e9f7f798bf937be60daf8ba3391b6509ed01a415
ArtyomKeith <kekulya00@gmail.com>
2024-12-02 11:35:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import folium
 import requests
 from streamlit_folium import folium_static
+from folium.plugins import MarkerCluster
 
 # Загружаем GeoJSON с GitHub
 url = "https://raw.githubusercontent.com/ArtyomKeith/lab13_iv/main/campus.geojson"
@@ -9,35 +10,42 @@ geojson_data = requests.get(url).json()
 
 # Функция для отображения карты с приближением на выбранное здание
 def create_map(selected_building=None):
-    # Инициализация карты
-    m = folium.Map(location=[51.1879, 71.4085], zoom_start=16, control_scale=True)
+    # Инициализация карты с возможностью выбора темной темы
+    m = folium.Map(location=[51.1879, 71.4085], zoom_start=16, control_scale=True, tiles='CartoDB dark_matter')
 
-    # Добавление GeoJSON данных на карту
-    folium.GeoJson(geojson_data, name="Campus").add_to(m)
+    # Добавляем кластер маркеров для улучшения взаимодействия с картой
+    marker_cluster = MarkerCluster().add_to(m)
+
+    # Добавление GeoJSON данных на карту (территория кампуса)
+    folium.GeoJson(geojson_data, name="Campus Area", style_function=lambda x: {'fillColor': '#ff7800', 'color': 'black', 'weight': 1, 'fillOpacity': 0.4}).add_to(m)
+
+    # Добавляем здания на карту
+    for feature in geojson_data['features']:
+        if feature['geometry']['type'] == 'Point':  # Добавляем только точки (здания)
+            coords = feature['geometry']['coordinates']
+            lat, lon = coords[1], coords[0]
+            building_name = feature['properties']['name']
+            building_description = feature['properties'].get('description', 'Описание отсутствует')
+            
+            # Выбираем иконку маркера в зависимости от типа здания
+            building_type = feature['properties'].get('type', 'default')
+            if building_type == 'administrative':
+                icon = folium.Icon(color='blue', icon='info-sign')
+            else:
+                icon = folium.Icon(color='green', icon='cloud')
+
+            # Создаем маркер с всплывающим окном, отображающим название и описание здания
+            marker = folium.Marker([lat, lon], popup=f"<b>{building_name}</b><br>{building_description}", icon=icon)
+            marker.add_to(marker_cluster)  # Добавляем маркер в кластер
 
     # Если выбрано здание, находим его и приближаем
     if selected_building:
         for feature in geojson_data['features']:
             if feature['properties']['name'] == selected_building:
-                # Извлекаем координаты здания
                 coords = feature['geometry']['coordinates']
-                
-                # Если это точка (Point), то координаты будут в формате [lon, lat]
                 if feature['geometry']['type'] == 'Point':
                     lat, lon = coords[1], coords[0]
-                    # Добавляем маркер на здание
-                    folium.Marker([lat, lon], popup=feature['properties']['name']).add_to(m)
-
-                    # Приближаем к этому зданию, чтобы здание было в центре экрана
                     m.fit_bounds([[lat - 0.002, lon - 0.002], [lat + 0.002, lon + 0.002]])
-
-                # Если это полигоны (например, здания с несколькими углами), то извлекаем координаты для этих полигонов
-                elif feature['geometry']['type'] == 'Polygon':
-                    # Получаем минимальные и максимальные координаты полигона для fit_bounds
-                    latitudes = [coord[1] for coord in coords[0]]
-                    longitudes = [coord[0] for coord in coords[0]]
-                    bounds = [[min(latitudes), min(longitudes)], [max(latitudes), max(longitudes)]]
-                    m.fit_bounds(bounds)
 
     # Отображаем карту
     folium_static(m)
